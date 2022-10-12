4120498d36589b62f807894b63b2788a07751a99
21A91A05A8 <97424469+21A91A05A8@users.noreply.github.com>
2022-10-12 12:24:08 +0530

    import java.util.Scanner; class Main{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double k=3.14;         int r=sc.nextInt();         System.out.printf("%.2f",k*r*r);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double k=3.14;
+        int r=sc.nextInt();
+        System.out.printf("%.2f",k*r*r);
+    }
+}
